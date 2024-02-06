90fad41e917c4b467aebc43e0298506a4bd8fae4
roseline oladiran <roselineoladiran97@gmail.com>
2024-02-06 00:43:20 -0800

    updated splash screen

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,54 +1,23 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:my_task/new.dart';
+// ignore: depend_on_referenced_packages
+import 'package:page_transition/page_transition.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
 
-class _HomePageState extends State<HomePage>  with SingleTickerProviderStateMixin{
-
-late final AnimationController _controller;
-
-@override
-void initState(){
-  super.initState();
-
-  _controller = AnimationController(
-    duration: const Duration(seconds: 2),
-    vsync: this);
-}
-
-@override
-void dispose() {
-  super.dispose();
-
-  _controller.dispose();
-}
-
-bool logo = false;
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            if (logo == false) {
-              logo = true;
-              _controller.forward();
-            } else{
-              logo = false;
-              _controller.reverse();
-            }
-          },
-          child: Lottie.network('https://app.lottiefiles.com/animation/bd154b6b-e6f9-4caa-9eff-674af573783a',
-          controller : _controller,
-          ),
-        )
-      ),
-    );
+    return AnimatedSplashScreen(splash: Lottie.asset('assets/loading.json'),
+    nextScreen: const MyWidget(),
+    splashIconSize: 150,
+    duration: 3000,
+    splashTransition: SplashTransition.sizeTransition,
+    pageTransitionType: PageTransitionType.leftToRightWithFade,
+    animationDuration: const Duration(seconds: 50),);
   }
 }
